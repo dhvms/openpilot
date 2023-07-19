b51425c92264a2b35a00b654604e61c088fadd76
yih666 <75866092+yih666@users.noreply.github.com>
2023-07-19 12:30:52 +0900

    Update hyundaican.py

--- selfdrive/car/hyundai/hyundaican.py
+++ selfdrive/car/hyundai/hyundaican.py
@@ -102,7 +102,7 @@ def create_hda_mfc(packer, active, CS, left_lane, right_lane):
     ldwSysState += 2
 
   values["HDA_LdwSysState"] = ldwSysState
-  values["HDA_USM"] = 1
+  values["HDA_USM"] = 2
   values["HDA_VSetReq"] = 100
   values["HDA_Icon_Wheel"] = 1 if active > 1 and CS.out.cruiseState.enabledAcc else 0
   values["HDA_Icon_State"] = 2 if active > 1 else 0
