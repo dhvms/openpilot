0c8564d99488fc10a01e27ecf4c0b49f6d677f53
dhvms <147835473+dhvms@users.noreply.github.com>
2025-06-05 15:29:07 +0900

    Update carcontroller.py

--- opendbc_repo/opendbc/car/hyundai/carcontroller.py
+++ opendbc_repo/opendbc/car/hyundai/carcontroller.py
@@ -17,9 +17,9 @@ LongCtrlState = structs.CarControl.Actuators.LongControlState
 
 # EPS faults if you apply torque while the steering angle is above 90 degrees for more than 1 second
 # All slightly below EPS thresholds to avoid fault
-MAX_ANGLE = 85
-MAX_ANGLE_FRAMES = 89
-MAX_ANGLE_CONSECUTIVE_FRAMES = 2
+MAX_ANGLE = 95
+MAX_ANGLE_FRAMES = 110
+MAX_ANGLE_CONSECUTIVE_FRAMES = 3
 
 
 def process_hud_alert(enabled, fingerprint, hud_control):
