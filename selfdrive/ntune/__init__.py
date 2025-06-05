87e1d8f04a95221fef3873ef237a134016a984b2
dhvms <147835473+dhvms@users.noreply.github.com>
2025-06-05 14:54:30 +0900

    Update __init__.py

--- selfdrive/ntune/__init__.py
+++ selfdrive/ntune/__init__.py
@@ -1,281 +1,103 @@
 import os
-import fcntl
-import signal
 import json
-import weakref
-from enum import Enum
-
-class LatType(Enum):
-  NONE = 0
-  INDI = 1
-  TORQUE = 2
-
-class GroupType:
-  NONE = "none"
-  INDI = "lat_indi"
-  TORQUE = "lat_torque_v4"
-  COMMON = "common"
-  SCC = "scc_v3"
 
 CONF_PATH = '/data/ntune/'
-CONF_LAT_INDI_FILE = '/data/ntune/'+GroupType.INDI+'.json'
-CONF_LAT_TORQUE_FILE = '/data/ntune/'+GroupType.TORQUE+'.json'
+GROUP_FILE = CONF_PATH + 'group.json'
+CTRL_FILE = CONF_PATH + 'ctrl.json'
+LAT_FILE = CONF_PATH + 'lat.json'
+LON_FILE = CONF_PATH + 'lon.json'
+PATH_FILE = CONF_PATH + 'path.json'
+CAMERA_FILE = CONF_PATH + 'camera.json'
 
-ntunes = {}
+CONF_LAT_INDI_FILE = CONF_PATH + 'lat_indi.json'
+CONF_LAT_TORQUE_FILE = CONF_PATH + 'lat_torque_v4.json'
+CONF_SCC_FILE = CONF_PATH + 'scc_v3.json'
+CONF_COMMON_FILE = CONF_PATH + 'common.json'
 
-def file_watch_handler(signum, frame):
-  global ntunes
-  for ntune in ntunes.values():
-    ntune.handle()
+def read_config_file(file_path):
+    try:
+        with open(file_path, 'r') as f:
+            return json.load(f)
+    except IOError:
+        return {}
 
-
-class nTune():
-
-  def get_ctrl(self):
-    return self.ctrl() if self.ctrl is not None else None
-
-  def __del__(self):
-    if self.group is None:
-      ntunes[self.key] = None
-    print('__del__', self)
-
-  def __init__(self, CP=None, ctrl=None, group=None):
-
-    self.invalidated = False
-    self.CP = CP
-    self.ctrl = weakref.ref(ctrl) if ctrl is not None else None
-    self.type = LatType.NONE
-    self.group = GroupType.NONE if group is None else group
-    self.config = {}
-    self.key = str(self)
-    self.disable_lateral_live_tuning = False
-
-    if "LatControlTorque" in str(type(ctrl)):
-      self.type = LatType.TORQUE
-      self.file = CONF_LAT_TORQUE_FILE
-    elif "LatControlINDI" in str(type(ctrl)):
-      self.type = LatType.INDI
-      self.file = CONF_LAT_INDI_FILE
-    else:
-      self.file = CONF_PATH + group + ".json"
-
-    if not os.path.exists(CONF_PATH):
-      os.makedirs(CONF_PATH)
-
-    self.read()
-
-    if self.group == GroupType.NONE:
-      ntunes[self.key] = self
+def ntune_common_get():
+    group = {}
+    ctrl = {}
+    lat = {}
+    lon = {}
+    path = {}
+    camera = {}
 
     try:
-      signal.signal(signal.SIGIO, file_watch_handler)
-      signal.siginterrupt(signal.SIGIO, False)
-      fd = os.open(CONF_PATH, os.O_RDONLY)
-      fcntl.fcntl(fd, fcntl.F_SETSIG, 0)
-      fcntl.fcntl(fd, fcntl.F_NOTIFY, fcntl.DN_MODIFY | fcntl.DN_CREATE | fcntl.DN_MULTISHOT)
-    except Exception as ex:
-      print("exception", ex)
-      pass
-
-  def handle(self):
-    try:
-      if os.path.getsize(self.file) > 0:
-        with open(self.file, 'r') as f:
-          self.config = json.load(f)
-
-        if self.checkValid():
-          self.write_config(self.config)
-
-        self.invalidated = True
-
-    except:
-      pass
-
-  def check(self):
-    if self.invalidated:
-      self.invalidated = False
-      self.update()
-
-  def read(self):
-    success = False
-    try:
-      if os.path.getsize(self.file) > 0:
-        with open(self.file, 'r') as f:
-          self.config = json.load(f)
-
-        if self.checkValid():
-          self.write_config(self.config)
-
-        self.update()
-        success = True
-    except:
-      pass
-
-    if not success:
-      try:
-        self.write_default()
-        with open(self.file, 'r') as f:
-          self.config = json.load(f)
-          if self.checkValid():
-            self.write_config(self.config)
-          self.update()
-      except:
+        with open(GROUP_FILE) as f:
+            group = json.load(f)
+    except IOError:
         pass
 
-  def checkValue(self, key, min_, max_, default_):
-    updated = False
-
-    if key not in self.config:
-      self.config.update({key: default_})
-      updated = True
-    elif min_ > self.config[key]:
-      self.config.update({key: min_})
-      updated = True
-    elif max_ < self.config[key]:
-      self.config.update({key: max_})
-      updated = True
-
-    return updated
-
-  def checkValid(self):
-
-    if self.type == LatType.INDI or self.group == GroupType.INDI:
-      return self.checkValidIndi()
-    elif self.type == LatType.TORQUE or self.group == GroupType.TORQUE:
-      return self.checkValidTorque()
-    elif self.group == GroupType.COMMON:
-      return self.checkValidCommon()
-    elif self.group == GroupType.SCC:
-      return self.checkValidSCC()
-
-    return False
-
-  def update(self):
-
-    if self.disable_lateral_live_tuning:
-      return
-
-    if self.type == LatType.INDI:
-      self.updateIndi()
-    elif self.type == LatType.TORQUE:
-      self.updateTorque()
-
-  def checkValidCommon(self):
-    updated = False
-
-    if self.checkValue("useLiveSteerRatio", 0., 1., 1.):
-      updated = True
-
-    if self.checkValue("steerRatio", 10.0, 20.0, 16.5):
-      updated = True
-
-    if self.checkValue("pathFactor", 0.9, 1.1, 0.95):
-      updated = True
-
-    return updated
-
-  def checkValidIndi(self):
-    updated = False
-
-    if self.checkValue("actuatorEffectiveness", 0.5, 3.0, 1.8):
-      updated = True
-    if self.checkValue("timeConstant", 0.5, 3.0, 1.4):
-      updated = True
-    if self.checkValue("innerLoopGain", 1.0, 5.0, 3.3):
-      updated = True
-    if self.checkValue("outerLoopGain", 1.0, 5.0, 2.8):
-      updated = True
-
-    return updated
-
-  def checkValidTorque(self):
-    updated = False
-
-    if self.checkValue("useSteeringAngle", 0., 1., 1.):
-      updated = True
-    if self.checkValue("latAccelFactor", 0.5, 4.5, 2.7):
-      updated = True
-    if self.checkValue("friction", 0.0, 0.2, 0.08):
-      updated = True
-    if self.checkValue("angle_deadzone_v2", 0.0, 2.0, 0.0):
-      updated = True
-
-    return updated
-
-  def checkValidSCC(self):
-    updated = False
-
-    if self.checkValue("longActuatorDelay", 0.1, 1.0, 0.5):
-      updated = True
-
-    if self.checkValue("accelTargetFactor", 0.7, 1.5, 1.0):
-      updated = True
-
-    if self.checkValue("decelTargetFactor", 0.7, 1.5, 1.0):
-      updated = True
-
-    if self.checkValue("comportBrake", 2.3, 2.7, 2.6):
-      updated = True
-
-    if self.checkValue("stopDistance", 4., 7., 5.0):
-      updated = True
-
-    return updated
-
-  def updateIndi(self):
-    indi = self.get_ctrl()
-    if indi is not None:
-      indi._RC = ([0.], [float(self.config["timeConstant"])])
-      indi._G = ([0.], [float(self.config["actuatorEffectiveness"])])
-      indi._outer_loop_gain = ([0.], [float(self.config["outerLoopGain"])])
-      indi._inner_loop_gain = ([0.], [float(self.config["innerLoopGain"])])
-      indi.steer_filter.update_alpha(indi.RC)
-      indi.reset()
-
-  def updateTorque(self):
-    torque = self.get_ctrl()
-    if torque is not None:
-      torque.use_steering_angle = float(self.config["useSteeringAngle"]) > 0.5
-      torque.steering_angle_deadzone_deg = float(self.config["angle_deadzone_v2"])
-      torque.torque_params.latAccelFactor = float(self.config["latAccelFactor"])
-      torque.torque_params.friction = float(self.config["friction"])
-
-  def read_cp(self):
-
     try:
-      if self.CP is not None:
-
-        if self.type == LatType.INDI:
-          pass
-        elif self.type == LatType.TORQUE:
-          self.config["useSteeringAngle"] = 1. if self.CP.lateralTuning.torque.useSteeringAngle else 0.
-          self.config["latAccelFactor"] = self.CP.lateralTuning.torque.latAccelFactor
-          self.config["friction"] = round(self.CP.lateralTuning.torque.friction, 3)
-
-          self.config["angle_deadzone_v2"] = round(self.CP.lateralTuning.torque.steeringAngleDeadzoneDeg, 1)
-        else:
-          self.config["useLiveSteerRatio"] = 1.
-          self.config["steerRatio"] = round(self.CP.steerRatio, 2)
-          self.config["steerRateCost"] = 0.35
-          self.config["steerActuatorDelay"] = 0.15
-          self.config["maxLatAccel"] = 2.2
-          self.config["friction"] = 0.1
-
-    except:
-      pass
-
-  def write_default(self):
-
-    try:
-      self.read_cp()
-      self.checkValid()
-      self.write_config(self.config)
-    except:
-      pass
-
-  def write_config(self, conf):
-    try:
-      with open(self.file, 'w') as f:
-        json.dump(conf, f, indent=2, sort_keys=False)
-        os.chmod(self.file, 0o666)
+        with open(CTRL_FILE) as f:
+            ctrl = json.load(f)
     except IOError:
+        pass
+
+    try:
+        with open(LAT_FILE) as f:
+            lat = json.load(f)
+    except IOError:
+        pass
+
+    try:
+        with open(LON_FILE) as f:
+            lon = json.load(f)
+    except IOError:
+        pass
+
+    try:
+        with open(PATH_FILE) as f:
+            path = json.load(f)
+    except IOError:
+        pass
+
+    try:
+        with open(CAMERA_FILE) as f:
+            camera = json.load(f)
+    except IOError:
+        pass
+
+    return {
+        'group': group,
+        'ctrl': ctrl,
+        'lat': lat,
+        'lon': lon,
+        'path': path,
+        'camera': camera,
+    }
+
+def ntune_lat_torque_get():
+    try:
+        with open(CONF_LAT_TORQUE_FILE) as f:
+            return json.load(f)
+    except IOError:
+        return {}
+
+def ntune_lat_indi_get():
+    try:
+        with open(CONF_LAT_INDI_FILE) as f:
+            return json.load(f)
+    except IOError:
+        return {}
+
+def ntune_scc_get():
+    try:
+        with open(CONF_SCC_FILE) as f:
+            return json.load(f)
+    except IOError:
+        return {}
+
+def ntune_common_params_get():
+    try:
+        with open(CONF_COMMON_FILE) as f:
+            return json.load(f)
+    except IOError:
+        return {}
